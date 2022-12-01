40cb71837ef93cfa7e94ed57cabc2ca0ec763f67
Arthur Lee <arthur@broadinstitute.org>
2022-12-01 18:49:38 -0500

    Update tobias_usage.sh

--- tobias_usage.sh
+++ tobias_usage.sh
@@ -1,11 +1,11 @@
 conda activate tobias
  
-TOBIAS ATACorrect --bam /home/arthur/tobias/CN7-10-5/CN7-e10-5_merged.bam --genome /home/arthur/tobias/GRCm38.p4.genome.fa --peaks /home/arthur/tobias/CN7-10-5/CN7_e10-5_mergedPeaks.bed --blacklist /home/arthur/tobias/mm10-blacklist.v2.bed --outdir corrected --cores 60
+TOBIAS ATACorrect --bam /home/tobias/CN7-10-5/CN7-e10-5_merged.bam --genome /home/arthur/tobias/GRCm38.p4.genome.fa --peaks /home/tobias/CN7-10-5/CN7_e10-5_mergedPeaks.bed --blacklist /home/tobias/mm10-blacklist.v2.bed --outdir --cores 60
 
-TOBIAS ATACorrect --bam /home/arthur/tobias/CN7-11-5/CN7-e11-5_merged.bam --genome /home/arthur/tobias/GRCm38.p4.genome.fa --peaks /home/arthur/tobias/CN7-11-5/CN7_e11-5_mergedPeaks.bed --blacklist /home/arthur/tobias/mm10-blacklist.v2.bed --outdir /home/arthur/tobias/CN7-11-5 --cores 60
+TOBIAS ATACorrect --bam /home/tobias/CN7-11-5/CN7-e11-5_merged.bam --genome /home/tobias/GRCm38.p4.genome.fa --peaks /home/tobias/CN7-11-5/CN7_e11-5_mergedPeaks.bed --blacklist /home/tobias/mm10-blacklist.v2.bed --outdir /home/tobias/CN7-11-5 --cores 60
 
-TOBIAS FootprintScores --signal /home/arthur/tobias/corrected/CN7-e10-5_merged_corrected.bw --regions /home/arthur/tobias/CN7-10-5/CN7_e10-5_mergedPeaks.bed --output CN7_10-5_footprints.bw --cores 60
+TOBIAS FootprintScores --signal /home/tobias/corrected/CN7-e10-5_merged_corrected.bw --regions /home/tobias/CN7-10-5/CN7_e10-5_mergedPeaks.bed --output CN7_10-5_footprints.bw --cores 60
 
-TOBIAS ScoreBed --bed /home/arthur/tobias/CN7-10-5/CN7_e10-5_mergedPeaks.bed --bigwigs /home/arthur/tobias/corrected/CN7-e10-5_merged_corrected.bw --output /home/arthur/tobias/CN7-10-5/merged_peaks_scored.bed
+TOBIAS ScoreBed --bed /home/tobias/CN7-10-5/CN7_e10-5_mergedPeaks.bed --bigwigs /home/tobias/corrected/CN7-e10-5_merged_corrected.bw --output /home/tobias/CN7-10-5/merged_peaks_scored.bed
 
-TOBIAS TFBScan --motifs /home/arthur/tobias/motifs.jaspar --fasta /home/arthur/tobias/GRCm38.p4.genome.fa --cores 60 --regions /home/arthur/tobias/CN7-10-5/CN7_e10-5_mergedPeaks.bed --add-region-columns --outdir /home/arthur/tobias/CN7-10-5/tfbscan_output
+TOBIAS TFBScan --motifs /home/tobias/motifs.jaspar --fasta /home/arthur/tobias/GRCm38.p4.genome.fa --cores 60 --regions /home/tobias/CN7-10-5/CN7_e10-5_mergedPeaks.bed --add-region-columns --outdir /home/tobias/CN7-10-5/tfbscan_output
